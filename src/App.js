17d11bf1a57ee14fa0b265a48aadc31f1523ffe4
陈英 <wb.chen.ying@redstarclouds.com>
2020-06-08 09:15:12 +0800

    login admin 路由

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React from 'react';
-import { Button } from 'antd';
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Login from "./view/login/Login";
+import Admin from "./view/admin/Admin";
 
 function App() {
   return (
-    <div className="App">
-     <h1>hello,react</h1>
-     <button>登录</button>
-     <Button type="primary">Primary Button</Button>
-    </div>
+    // <div className="App">
+    //  <h1>hello,react</h1>
+    //  <button>登录</button>
+    //  <Button type="primary">Primary Button</Button>
+    // </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/login" component={Login} />
+        <Route path="/" component={Admin} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
